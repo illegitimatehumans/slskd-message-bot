d7d473b42999a4949f14a528dd58ed956eb775d1
illegitimatehumans <barrettmontreal@gmail.com>
2026-07-09 19:50:52 -0400

    Automatically create message.txt from example

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ import time
 from app.config import CHECK_INTERVAL
 from app.services.monitor import get_active_users
 from app.services.processor import process
+from app.services.messenger import ensure_message
 
 print()
 print("=" * 70)
 print(" OwlTV slskd Bot")
 print("=" * 70)
+
+ensure_message()
 
 while True:
 
